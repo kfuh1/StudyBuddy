bb9149e0771c3dbba07937d24d9f7f5f56e840df
John Behnke <behnkej@me.com>
2015-07-18 21:38:36 -0400

    Added rudimentary ListView with dummy data and a Floating Action Button

--- StudyBuddy/app/src/main/java/android/example/com/studdybuddy/MainActivity.java
+++ StudyBuddy/app/src/main/java/android/example/com/studdybuddy/MainActivity.java
@@ -1,22 +1,15 @@
 package android.example.com.studdybuddy;
 
-import java.util.Locale;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -49,6 +42,7 @@ public class MainActivity extends ActionBarActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+
 
     }
 
@@ -90,13 +84,13 @@ public class MainActivity extends ActionBarActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return StudySessionFragment.newInstance(position + 1);
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
@@ -107,44 +101,12 @@ public class MainActivity extends ActionBarActivity {
                     return getString(R.string.title_section1).toUpperCase(l);
                 case 1:
                     return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
+
             }
             return null;
         }
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
 }
---
+++ StudyBuddy/app/src/main/java/android/example/com/studdybuddy/StudySessionFragment.java
@@ -0,0 +1,71 @@
+package android.example.com.studdybuddy;
+
+/**
+ * Created by John on 7/18/15.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class StudySessionFragment extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    //TODO: Make this adapter array take in a custom "Study Session" object
+
+    private ArrayAdapter<String> mStudySessionAdapter;
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static StudySessionFragment newInstance(int sectionNumber) {
+        StudySessionFragment fragment = new StudySessionFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public StudySessionFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        mStudySessionAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_studysession,
+                R.id.list_item_studysession_name_textview,
+                new ArrayList<String>()
+        );
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Get a reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listView_studdysession);
+        listView.setAdapter(mStudySessionAdapter);
+
+//TODO: Set up proper floatingactionbutton to launch a "create" view to make a session
+    mStudySessionAdapter.add("Hello World");
+        mStudySessionAdapter.add("Hello World");
+        mStudySessionAdapter.add("Hello World");
+        mStudySessionAdapter.add("Hello World");
+
+
+        return rootView;
+    }
+}
+
