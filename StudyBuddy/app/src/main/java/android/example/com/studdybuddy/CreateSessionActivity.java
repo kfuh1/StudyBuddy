ddb5b065049c02f7c98a4a4cd1c54d9e131efb2b
kfuh1 <kfuh@andrew.cmu.edu>
2015-08-07 13:45:10 -0400

    Add initial files for making CreateSession screen

---
+++ StudyBuddy/app/src/main/java/android/example/com/studdybuddy/CreateSessionActivity.java
@@ -0,0 +1,15 @@
+package android.example.com.studdybuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Kathleen on 8/7/2015.
+ */
+public class CreateSessionActivity extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_session);
+    }
+}
---
+++ StudyBuddy/app/src/main/java/android/example/com/studdybuddy/CreateSessionActivityFragment.java
@@ -0,0 +1,21 @@
+package android.example.com.studdybuddy;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Kathleen on 8/7/2015.
+ */
+public class CreateSessionActivityFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+
+        View rootView = inflater.inflate(R.layout.fragment_create_session, container, false);
+        return rootView;
+
+    }
+}
