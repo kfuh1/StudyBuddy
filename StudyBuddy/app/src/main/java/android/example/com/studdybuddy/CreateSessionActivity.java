2fbb1dbe8444313c844b0893b8ce32eeaa8817ce
kfuh1 <kfuh@andrew.cmu.edu>
2015-08-11 14:45:08 -0700

    Update create session screen with subjects and add write to Parse database

--- StudyBuddy/app/src/main/java/android/example/com/studdybuddy/CreateSessionActivity.java
+++ StudyBuddy/app/src/main/java/android/example/com/studdybuddy/CreateSessionActivity.java
@@ -1,38 +1,76 @@
 package android.example.com.studdybuddy;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
-import android.app.AlertDialog;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
 /**
  * Created by Kathleen on 8/7/2015.
  */
-public class CreateSessionActivity extends ActionBarActivity {
+public class CreateSessionActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
+    private boolean mIsValidSession;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_session);
+
+        /* set dropdown list of meeting time choice */
         Spinner timeSpinner = (Spinner) findViewById(R.id.meeting_time_spinner);
-
-
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter.createFromResource(this,
                 R.array.meeting_time_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        timeSpinner.setAdapter(adapter);
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
+
+        Spinner subjectSpinner = (Spinner) findViewById(R.id.subject_spinner);
+        ArrayAdapter<CharSequence> subjectAdapter = ArrayAdapter.createFromResource(this,
+                R.array.subject_array, android.R.layout.simple_spinner_item);
+        subjectAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        subjectSpinner.setAdapter(subjectAdapter);
     }
 
     /* Method that gets called when SAVE button is clicked*/
     public void saveCreateData(View view){
-        //TODO: save the data in the create session screen
+        ParseObject sessionObject = new ParseObject("StudySession");
+        ParseUser user = ParseUser.getCurrentUser();
+        EditText titleText = (EditText) findViewById(R.id.title_text);
+        EditText descText = (EditText) findViewById(R.id.desc_text);
+        EditText locationText = (EditText) findViewById(R.id.location_text);
+        Spinner timeSpinner = (Spinner) findViewById(R.id.meeting_time_spinner);
+        Spinner subjectSpinner = (Spinner) findViewById(R.id.subject_spinner);
+        String timeToMeet;
+        String subjectName;
+        if(timeSpinner.getSelectedItem() == null){
+            timeToMeet = "";
+        }
+        else{
+            timeToMeet = timeSpinner.getSelectedItem().toString();
+        }
+        if(subjectSpinner.getSelectedItem() == null){
+            subjectName = "";
+        }
+        else{
+            subjectName = subjectSpinner.getSelectedItem().toString();
+        }
 
-        //debug toast
-        Toast.makeText(getApplicationContext(), "saving data!", Toast.LENGTH_SHORT).show();
+        sessionObject.put("sessionName",titleText.getText());
+        sessionObject.put("sessionDesc", descText.getText());
+        sessionObject.put("locationName", locationText.getText());
+        sessionObject.put("subjectType", subjectName);
+        sessionObject.put("timeToMeet", timeToMeet);
+        sessionObject.put("user", user);
+
+        sessionObject.saveInBackground();
     }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event){
@@ -63,5 +101,15 @@ public class CreateSessionActivity extends ActionBarActivity {
                 })
                 .show();
 
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
