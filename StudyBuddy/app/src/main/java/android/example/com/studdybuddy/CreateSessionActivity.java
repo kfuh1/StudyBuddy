76a878c275a08e84a522757d9cc7b9848074ff0b
kfuh1 <kfuh@andrew.cmu.edu>
2015-08-11 13:22:03 -0700

    Close dialog box

--- StudyBuddy/app/src/main/java/android/example/com/studdybuddy/CreateSessionActivity.java
+++ StudyBuddy/app/src/main/java/android/example/com/studdybuddy/CreateSessionActivity.java
@@ -1,10 +1,14 @@
 package android.example.com.studdybuddy;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-
+import android.widget.Toast;
+import android.app.AlertDialog;
 /**
  * Created by Kathleen on 8/7/2015.
  */
@@ -21,5 +25,43 @@ public class CreateSessionActivity extends ActionBarActivity {
                 R.array.meeting_time_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         timeSpinner.setAdapter(adapter);
+    }
+
+    /* Method that gets called when SAVE button is clicked*/
+    public void saveCreateData(View view){
+        //TODO: save the data in the create session screen
+
+        //debug toast
+        Toast.makeText(getApplicationContext(), "saving data!", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    protected void exitByBackKey(){
+        AlertDialog alertbox = new AlertDialog.Builder(this)
+                .setMessage("Do you want to save your changes to the application?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    // do something when the button is clicked
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        saveCreateData(findViewById(R.id.fragment_create));
+                        finish();
+                        //close();
+
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                    // do something when the button is clicked
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                })
+                .show();
+
     }
 }
